d6fa2e7bb83a7e95504351d18e250a5b69877d81
Toby S. Cubitt <toby-undo-tree@dr-qubit.org>
2013-08-05 15:02:45 +0100

    Reinstate accidentally deleted file header.

--- undo-tree.el
+++ undo-tree.el
@@ -1,4 +1,20 @@
+;;; undo-tree.el --- Treat undo history as a tree  -*- lexical-binding: t; -*-
 
+;; Copyright (C) 2009-2012  Free Software Foundation, Inc
+
+;; Author: Toby Cubitt <toby-undo-tree@dr-qubit.org>
+;; Version: 0.6.3
+;; Keywords: convenience, files, undo, redo, history, tree
+;; URL: http://www.dr-qubit.org/emacs.php
+;; Repository: http://www.dr-qubit.org/git/undo-tree.git
+
+;; This file is part of Emacs.
+;;
+;; This file is free software: you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free
+;; Software Foundation, either version 3 of the License, or (at your option)
+;; any later version.
+;;
 ;; This program is distributed in the hope that it will be useful, but WITHOUT
 ;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 ;; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
